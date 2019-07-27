0ced864bedb452937090d616991ea8ea1436c23a
Kirill Ivanchenko <kirill.ivanchenko@slcode.com>
2019-07-28 00:07:31 +0300

    Created scripts project for manage database

---
+++ graph-master.scripts/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace graph_master.scripts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
