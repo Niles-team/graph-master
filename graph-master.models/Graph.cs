706433fba98b2e94b094b508ad0267e241a4018c
Kirill Ivanchenko <kirill.ivanchenko@slcode.com>
2019-08-03 15:38:53 +0300

    Moved models fields to upper camel case

--- graph-master.models/Graph.cs
+++ graph-master.models/Graph.cs
@@ -4,24 +4,24 @@ namespace graph_master.models
 {
     public class Graph
     {
-        public int id { get; set; }
-        public int? userId { get; set; }
-        public int? teamId { get; set; }
-        public string name { get; set; }
-        public List<Node> nodes { get; set; }
-        public List<Link> links { get; set; }
+        public int Id { get; set; }
+        public int? UserId { get; set; }
+        public int? TeamId { get; set; }
+        public string Name { get; set; }
+        public List<Node> Nodes { get; set; }
+        public List<Link> Links { get; set; }
     }
 
     public class Node
     {
-        public int id { get; set; }
-        public int graphId { get; set; }
-        public string data { get; set; }
+        public int Id { get; set; }
+        public int GraphId { get; set; }
+        public string Data { get; set; }
     }
     public class Link
     {
-        public int id { get; set; }
-        public int graphId { get; set; }
-        public string date { get; set; }
+        public int Id { get; set; }
+        public int GraphId { get; set; }
+        public string Data { get; set; }
     }
 }
--- graph-master.models/User.cs
+++ graph-master.models/User.cs
@@ -2,16 +2,16 @@ namespace graph_master.models
 {
     public class User
     {
-        public int id { get; set; }
-        public string userName { get; set; }
-        public string password { get; set; }
-        public string firstName { get; set; }
-        public string lastName { get; set; }
-        public string email { get; set; }
-        public int? teamId { get; set; }
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int? TeamId { get; set; }
     }
     public class UserAuthenticate : User
     {
-        public string token { get; set; }
+        public string Token { get; set; }
     }
 }
