4dd5d754ccfbdf68da2a2a072232d0854ec3f8ca
Kirill Ivanchenko <kirill.ivanchenko@slcode.com>
2019-08-07 05:37:23 +0300

    Added user controller; Registered user and email services in web api

---
+++ graph-master.api/Controllers/UsersController.cs
@@ -0,0 +1,47 @@
+using System.Threading.Tasks;
+using graph_master.models;
+using graph_master.services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace graph_master.api.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private UserService _userService;
+
+        public UsersController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("sign-up")]
+        public async Task<IActionResult> SignUp([FromBody]User model)
+        {
+            var result = await _userService.CreateUser(model);
+
+            if(result == null) {
+                return BadRequest(new { message = "There was some errors with user sign up" });
+            }
+
+            return Ok(result);
+        }
+
+        [AllowAnonymous]
+        [HttpPost("sign-in")]
+        public async Task<IActionResult> SignIn([FromBody]UserSignIn model)
+        {
+            var result = await _userService.SignIn(model.UserName, model.Password);
+
+            if (result == null)
+                return Ok(new { message = "User name or password is incorrect" });
+
+            return Ok(result);
+        }
+    }
+
+}
--- graph-master.api/Startup.cs
+++ graph-master.api/Startup.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using graph_master.models.helpers;
 using graph_master.data;
 using graph_master.data.interfaces;
 using graph_master.models.enums;
+using graph_master.services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -13,6 +15,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Http;
 
 namespace graph_master.api
 {
@@ -28,20 +34,65 @@ namespace graph_master.api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options => {
+                options.AddDefaultPolicy(builder => {
+                    builder
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowAnyOrigin();
+                });
+            });
+
+            services.AddHttpContextAccessor();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             string connectionString = Configuration.GetConnectionString("graph-master");
+            // configure strongly typed settings objects
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            // configure jwt authentication
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
             
+            IDaoFactory factory = DaoFactories.GetFactory(DataProvider.AdoNet, connectionString);
             Environment.SetEnvironmentVariable("connectionString", connectionString);
             
-            services.AddScoped<IUserDao>(provider => {
-                var factory = DaoFactories.GetFactory(DataProvider.AdoNet, connectionString);
-                return factory.UserDao;
+            services.AddScoped<EmailService>(provider => {
+                var appSettingsResolved = provider.GetService<IOptions<AppSettings>>();
+                return new EmailService(appSettingsResolved.Value); 
+            });
+
+            services.AddTransient<UserService>(provider => {
+                var appSettingsResolved = provider.GetService<IOptions<AppSettings>>();
+                var emailService = provider.GetService<EmailService>();
+                var httpAccessor = provider.GetService<IHttpContextAccessor>();
+                return new UserService(factory.UserDao, emailService, appSettingsResolved.Value, httpAccessor);
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -52,7 +103,8 @@ namespace graph_master.api
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            app.UseAuthentication();
+
             app.UseMvc();
         }
     }
