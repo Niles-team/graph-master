6de89c8118a88319370433b73a1e569df85de993
Kirill Ivanchenko <kirill.ivanchenko@slcode.com>
2019-08-07 04:54:45 +0300

    Changed routes and errors redirection; Added private and public routes export

--- graph-master/src/routes.tsx
+++ graph-master/src/routes.tsx
@@ -1,19 +1,23 @@
 import * as React from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Redirect } from "react-router-dom";
 
-import { UnauthorizedRedirect } from "./components/common";
-import { Layout } from "./components/layouts";
+import { ErrorRedirect } from "./components/common";
 import { routes as localRoutes } from "./sharedConstants";
+import { PrivateRoute, PublicRoute } from "./components/routeComponents";
+import { SignIn } from "./components/SignIn";
+import { SignUp } from "./components/SignUp";
+import { Error as ErrorPage } from "./components/Error";
 
-const dashboard = localRoutes.find(o=> o.name === 'Dashboard');
+const dashboard = localRoutes.find(o => o.name === 'Dashboard');
 
 export const routes = (
-    <UnauthorizedRedirect>
-        <Layout>
-            <Switch>
-                {localRoutes.map(route => <Route exact={route.exact} path={route.path} component={route.component} />)}
-                <Redirect to={dashboard.path} />
-            </Switch>
-        </Layout>
-    </UnauthorizedRedirect>
+    <ErrorRedirect>
+        <Switch>
+            {localRoutes.map(route => <PrivateRoute exact={route.exact} path={route.path} component={route.component} />)}
+            <PublicRoute restricted={true} exact path="/sign-in" component={SignIn} />
+            <PublicRoute restricted={true} exact path="/sign-up" component={SignUp} />
+            <PublicRoute restricted={false} exact path="/error" component={ErrorPage} />
+            <Redirect to={dashboard.path} />
+        </Switch>
+    </ErrorRedirect>
 );
