67d6b8f0fe348a7b3628f2ded17d679218952eef
Kirill Ivanchenko <kirill.ivanchenko@slcode.com>
2019-08-08 06:37:56 +0300

    Added confirm user method to front-end user service

--- graph-master/src/services/userService.ts
+++ graph-master/src/services/userService.ts
@@ -2,19 +2,19 @@ import { handleJsonResponse, ResponseHandler } from "../utils";
 import { AuthenticatedUser, User } from "../models";
 
 class UserService {
-    public async signIn(userName: string, password: string) {
+    public async signIn(userName: string, password: string): Promise<AuthenticatedUser> {
         return fetch('api/users/sign-in', {
-            method: 'POST',            
+            method: 'POST',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({ userName, password })
         })
-        .then(handleJsonResponse as ResponseHandler<AuthenticatedUser>);
+            .then(handleJsonResponse as ResponseHandler<AuthenticatedUser>);
     }
 
-    public async signUp(userName: string, password: string, firstName: string, lastName: string, email: string) {
+    public async signUp(userName: string, password: string, firstName: string, lastName: string, email: string): Promise<User> {
         return fetch('api/users/sign-up', {
             method: 'POST',
             headers: {
@@ -29,7 +29,19 @@ class UserService {
                 email
             })
         })
-        .then(handleJsonResponse as ResponseHandler<User>);
+            .then(handleJsonResponse as ResponseHandler<User>);
+    }
+
+    public async confirmUser(confirmCode: string): Promise<User> {
+        return fetch('api/users/confirm-user', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ code: confirmCode })
+        })
+            .then(handleJsonResponse as ResponseHandler<User>);
     }
 }
 
