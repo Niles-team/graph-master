9151d6ea80fe3f733269acc00944c3b34eecf34c
Kirill Ivanchenko <kirill.ivanchenko@slcode.com>
2019-08-09 06:31:57 +0300

    Added ui error messages

--- graph-master/src/components/SignUp.tsx
+++ graph-master/src/components/SignUp.tsx
@@ -4,7 +4,7 @@ import { WithStyles, withStyles } from "@material-ui/styles";
 
 import { mergeStyles } from "../utils";
 import { authenticateStyles, commonStyles, layoutStyles } from "../mui-theme";
-import { Grid, Avatar, Paper, TextField, Typography, FormControlLabel, Button, CircularProgress, Link, Checkbox } from "@material-ui/core";
+import { Grid, Avatar, Paper, TextField, Typography, FormControlLabel, Button, CircularProgress, Link, Checkbox, FormControl, FormHelperText } from "@material-ui/core";
 import { Timeline } from "@material-ui/icons";
 import { User, UserResponse } from "../models";
 import { userService } from "../services";
@@ -15,10 +15,13 @@ interface Props extends WithStyles<typeof styles> { }
 
 interface State {
     userName: string;
+    userNameError?: string;
     password: string;
+    passwordError?: string;
     firstName: string;
     lastName: string;
     email: string;
+    emailError?: string;
     message: string;
     loading: boolean;
 }
@@ -85,26 +88,49 @@ class SignUpBase extends React.Component<Props, State> {
         });
     }
 
+    private validateUserName = async () => {
+        const { userName } = this.state;
+        const userNameError = await userService.validateUserName(userName);
+        this.setState({ userNameError });
+    }
+
+    private validateEmail = async () => {
+        const { email } = this.state;
+        const emailError = await userService.validateEmail(email);
+        this.setState({ emailError });
+    }
+
+    private validatePassword = () => {
+        const { password } = this.state;
+        const passwordError = password.length < 8 ? 'Password is to short' : '';
+        this.setState({ passwordError });
+    }
+
     private validateCredentials(): boolean {
-        const { userName, password, firstName, lastName, email } = this.state;
-        return userName && userName.length >= 5
-            && password && password.length >= 8
+        const { userNameError, passwordError, firstName, lastName, emailError } = this.state;
+        return !Boolean(userNameError)
+            && !Boolean(passwordError)
+            && !Boolean(emailError)
             && firstName && firstName.length > 0
-            && lastName && lastName.length > 0
-            && email && email.length > 0;
+            && lastName && lastName.length > 0;
     }
 
     render() {
         const { classes } = this.props;
         const {
             userName,
+            userNameError,
             password,
+            passwordError,
             firstName,
             lastName,
             email,
+            emailError,
             message,
             loading
         } = this.state;
+
+        const isCredentialsValid = this.validateCredentials();
 
         return (
             <Grid container component="main" className={classes.root}>
@@ -118,76 +144,92 @@ class SignUpBase extends React.Component<Props, State> {
                             Sign in to Graph-Master
                         </Typography>
                         <form className={classes.form} onSubmit={this.handleSubmit}>
-                            <TextField
-                                variant="outlined"
-                                margin="normal"
-                                required
-                                fullWidth
-                                placeholder="Enter your user name"
-                                id="userName"
-                                label="User Name"
-                                name="userName"
-                                autoComplete="username"
-                                value={userName}
-                                onChange={this.handleUserNameChange}
-                                autoFocus
-                            />
-                            <TextField
-                                variant="outlined"
-                                margin="normal"
-                                required
-                                fullWidth
-                                name="password"
-                                label="Password"
-                                type="password"
-                                id="password"
-                                autoComplete="new-password"
-                                value={password}
-                                onChange={this.handlePasswordChange}
-                            />
-                            <TextField
-                                variant="outlined"
-                                margin="normal"
-                                required
-                                fullWidth
-                                name="firstname"
-                                label="First name"
-                                id="firstname"
-                                autoComplete="given-name"
-                                value={firstName}
-                                onChange={this.handleFirstNameChange}
-                            />
-                            <TextField
-                                variant="outlined"
-                                margin="normal"
-                                required
-                                fullWidth
-                                name="lastname"
-                                label="Last name"
-                                id="lastname"
-                                autoComplete="family-name"
-                                value={lastName}
-                                onChange={this.handleLastNameChange}
-                            />
-                            <TextField
-                                variant="outlined"
-                                margin="normal"
-                                required
-                                fullWidth
-                                name="email"
-                                label="E-mail address"
-                                id="email"
-                                autoComplete="email"
-                                value={email}
-                                onChange={this.handleEmailChange}
-                            />
+                            <FormControl fullWidth error={Boolean(userNameError)}>
+                                <TextField
+                                    variant="outlined"
+                                    margin="normal"
+                                    required
+                                    placeholder="Enter your user name"
+                                    id="userName"
+                                    label="User Name"
+                                    name="userName"
+                                    autoComplete="username"
+                                    value={userName}
+                                    onChange={this.handleUserNameChange}
+                                    onBlur={this.validateUserName}
+                                    autoFocus
+                                />
+                                {Boolean(userNameError) && <FormHelperText>{userNameError}</FormHelperText>}
+                            </FormControl>
+                            <FormControl fullWidth error={Boolean(passwordError)}>
+                                <TextField
+                                    variant="outlined"
+                                    margin="normal"
+                                    required
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                    autoComplete="new-password"
+                                    value={password}
+                                    onChange={this.handlePasswordChange}
+                                    onBlur={this.validatePassword}
+                                />
+                                {Boolean(passwordError) && <FormHelperText>{passwordError}</FormHelperText>}
+                            </FormControl>
+                            <FormControl fullWidth error={!Boolean(firstName)}>
+                                <TextField
+                                    variant="outlined"
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    name="firstname"
+                                    label="First name"
+                                    id="firstname"
+                                    autoComplete="given-name"
+                                    value={firstName}
+                                    onChange={this.handleFirstNameChange}
+                                />
+                                {!Boolean(firstName) && <FormHelperText>First name is required</FormHelperText>}
+                            </FormControl>
+                            <FormControl fullWidth error={!Boolean(lastName)}>
+                                <TextField
+                                    variant="outlined"
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    name="lastname"
+                                    label="Last name"
+                                    id="lastname"
+                                    autoComplete="family-name"
+                                    value={lastName}
+                                    onChange={this.handleLastNameChange}
+                                />
+                                {!Boolean(lastName) && <FormHelperText>Last name is required</FormHelperText>}
+                            </FormControl>
+                            <FormControl fullWidth error={Boolean(emailError)}>
+                                <TextField
+                                    variant="outlined"
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    name="email"
+                                    label="E-mail address"
+                                    id="email"
+                                    autoComplete="email"
+                                    value={email}
+                                    onChange={this.handleEmailChange}
+                                    onBlur={this.validateEmail}
+                                />
+                                {Boolean(emailError) && <FormHelperText>{emailError}</FormHelperText>}
+                            </FormControl>
                             <div className={classes.submit}>
                                 <Button
                                     type="submit"
                                     fullWidth
                                     variant="contained"
                                     color="primary"
-                                    disabled={loading || !this.validateCredentials()}
+                                    disabled={loading || !isCredentialsValid}
                                 >
                                     <div>Sign In</div>
                                     {loading && <CircularProgress size={24} className={classes.buttonProgress} />}
