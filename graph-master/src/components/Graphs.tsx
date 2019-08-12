cefbe187c21102a7fe391e8e3093f5c3ec9c34f4
Kirill Ivanchenko <kirill.ivanchenko@slcode.com>
2019-08-12 05:48:58 +0300

    Added user graphs paper;

--- graph-master/src/components/Graphs.tsx
+++ graph-master/src/components/Graphs.tsx
@@ -1,15 +1,66 @@
 import * as React from "react";
+import clsx from "clsx";
 
-import { withStyles } from "@material-ui/core";
+import { withStyles, Paper, Toolbar, Typography, Tooltip, IconButton, WithStyles } from "@material-ui/core";
+import { Delete, FilterList } from "@material-ui/icons";
+
 import { mergeStyles } from "../utils/mergeStyles";
-import { IsUnderConstruction } from "./common";
 
 const styles = mergeStyles();
 
-class GraphsBase extends React.Component {
+interface State {
+    selected: number;
+}
+
+class GraphsBase extends React.Component<WithStyles<typeof styles>, State> {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selected: 0
+        }
+    }
+
     render() {
+        const {classes} = this.props;
+        const {selected} = this.state;
+
         return (
-            <IsUnderConstruction/>
+            <Paper>
+                <Toolbar
+                    className={clsx(classes.root, {
+                        [classes.highlight]: selected > 0,
+                    })}
+                >
+                    <div className={classes.title}>
+                        {selected > 0 ? (
+                            <Typography color="inherit" variant="subtitle1">
+                                {selected} selected
+                            </Typography>
+                        ) : (
+                                <Typography variant="h6" id="tableTitle">
+                                    Graphs
+                            </Typography>
+                            )}
+                    </div>
+                    <div className={classes.spacer} />
+                    <div className={classes.actions}>
+                        {selected > 0 ? (
+                            <Tooltip title="Delete">
+                                <IconButton aria-label="delete">
+                                    <Delete />
+                                </IconButton>
+                            </Tooltip>
+                        ) : (
+                                <Tooltip title="Filter list">
+                                    <IconButton aria-label="filter list">
+                                        <FilterList />
+                                    </IconButton>
+                                </Tooltip>
+                            )}
+                    </div>
+                </Toolbar>
+            </Paper>
         );
     }
 }
