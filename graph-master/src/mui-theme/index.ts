528da56b414e772e6a5c3ba3e337ce17b7742661
Kirill Ivanchenko <kirill.ivanchenko@slcode.com>
2019-08-17 20:47:25 +0300

    Created table styles;

--- graph-master/src/mui-theme/index.ts
+++ graph-master/src/mui-theme/index.ts
@@ -1,3 +1,4 @@
 export * from './layoutStyles'; 
 export * from './authenticateStyles';
 export * from './commonStyles';
+export * from './tableStyles';
---
+++ graph-master/src/mui-theme/tableStyles.ts
@@ -0,0 +1,44 @@
+import { lighten } from "@material-ui/core/styles";
+
+export const tableStyles = theme => ({
+    toolBarRoot: {
+        paddingLeft: theme.spacing(2),
+        paddingRight: theme.spacing(1),
+    },
+    highlight:
+        theme.palette.type === 'light'
+            ? {
+                color: theme.palette.secondary.main,
+                backgroundColor: lighten(theme.palette.secondary.light, 0.85),
+            }
+            : {
+                color: theme.palette.text.primary,
+                backgroundColor: theme.palette.secondary.dark,
+            },
+    spacer: {
+        flex: '1 1 100%',
+    },
+    actions: {
+        color: theme.palette.text.secondary,
+    },
+    title: {
+        flex: '0 0 auto',
+    },
+    table: {
+      minWidth: 750,
+    },
+    tableWrapper: {
+      overflowX: 'auto',
+    },
+    visuallyHidden: {
+      border: 0,
+      clip: 'rect(0 0 0 0)',
+      height: 1,
+      margin: -1,
+      overflow: 'hidden',
+      padding: 0,
+      position: 'absolute',
+      top: 20,
+      width: 1,
+    },
+});
