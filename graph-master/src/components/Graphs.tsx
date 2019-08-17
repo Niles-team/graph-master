1968d9110aa55972cae4a5554d91847934a77f44
Kirill Ivanchenko <kirill.ivanchenko@slcode.com>
2019-08-17 20:48:34 +0300

    Added new react material table for test data;

--- graph-master/src/components/Graphs.tsx
+++ graph-master/src/components/Graphs.tsx
@@ -1,15 +1,14 @@
 import * as React from "react";
-import clsx from "clsx";
 
-import { withStyles, Paper, Toolbar, Typography, Tooltip, IconButton, WithStyles } from "@material-ui/core";
-import { Delete, FilterList } from "@material-ui/icons";
+import { withStyles, Paper, WithStyles } from "@material-ui/core";
 
 import { mergeStyles } from "../utils/mergeStyles";
+import { ReactTable } from "./common";
+import { Column } from "../models";
 
 const styles = mergeStyles();
 
 interface State {
-    selected: number;
 }
 
 class GraphsBase extends React.Component<WithStyles<typeof styles>, State> {
@@ -22,44 +21,37 @@ class GraphsBase extends React.Component<WithStyles<typeof styles>, State> {
     }
 
     render() {
-        const {classes} = this.props;
-        const {selected} = this.state;
+        const { classes } = this.props;
+
+        const columns: Column[] = [{
+            id: 'name',
+            label: 'Name',
+            align: 'justify',
+            disableSort: false,
+            disablePadding: true
+        }, {
+            id: 'date',
+            label: 'Date',
+            align: 'right',
+            disableSort: true,
+            disablePadding: false
+        }];
+
+        const data = [{
+            name: 'new graph',
+            date: new Date().toISOString(),
+            selected: true
+        }];
 
         return (
             <Paper>
-                <Toolbar
-                    className={clsx(classes.root, {
-                        [classes.highlight]: selected > 0,
-                    })}
-                >
-                    <div className={classes.title}>
-                        {selected > 0 ? (
-                            <Typography color="inherit" variant="subtitle1">
-                                {selected} selected
-                            </Typography>
-                        ) : (
-                                <Typography variant="h6" id="tableTitle">
-                                    Graphs
-                            </Typography>
-                            )}
-                    </div>
-                    <div className={classes.spacer} />
-                    <div className={classes.actions}>
-                        {selected > 0 ? (
-                            <Tooltip title="Delete">
-                                <IconButton aria-label="delete">
-                                    <Delete />
-                                </IconButton>
-                            </Tooltip>
-                        ) : (
-                                <Tooltip title="Filter list">
-                                    <IconButton aria-label="filter list">
-                                        <FilterList />
-                                    </IconButton>
-                                </Tooltip>
-                            )}
-                    </div>
-                </Toolbar>
+                <ReactTable 
+                    data={data} 
+                    title={"Your Graphs"} 
+                    appendCheckbox={true} 
+                    columns={columns} 
+                    dense={false}
+                />
             </Paper>
         );
     }
